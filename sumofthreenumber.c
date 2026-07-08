0d79aa5436da5707c31a3388512acb6bace738fb
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-07-08 19:19:16 +0530

    Add sum of three numbers program in C

---
+++ sumofthreenumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+	int n1,n2,n3,sum;
+	printf("enter first number");
+	scanf("%d",&n1);
+	printf("enter second number");
+	scanf("%d",&n2);
+	printf("enter third number");
+	scanf("%d",&n3);   
+	sum=n1+n2+n3;
+	printf("the sum of three numbers is %d",sum);
+	return 0;
+}
+
