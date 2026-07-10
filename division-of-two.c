2ead40bdb8f0ee90389d7f95d5a068b8953e3a17
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-07-10 21:23:35 +0530

    division of two program in c

---
+++ division-of-two.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int n1,n2,divide;
+	printf ("enter the first number");
+	scanf ("%d",&n1);
+	printf("enter the second number");
+	scanf("%d",n2);
+	divide=n1/n2;
+	printf("divide of two number is %d",divide);
+	return 0 ;
+}
