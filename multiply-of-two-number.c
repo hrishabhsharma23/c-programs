07b879d78f91728debfb1d4706e5781b9a35715a
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-07-11 16:48:17 +0530

    multiply of two number program in c

---
+++ multiply-of-two-number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int n1,n2,multiply;
+	printf ("enter the first number");
+	scanf ("%d",&n1);
+	printf("enter the second number");
+	scanf("%d",&n2);
+	multiply=n1*n2;
+	printf("multiply of two number is %d",multiply);
+	return 0 ;
+}
