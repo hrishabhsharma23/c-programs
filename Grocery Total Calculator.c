b09b0b527576b47b105eb60ce44529680798d7be
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-07-13 13:15:07 +0530

    Grocery Total Calculator program in c

---
+++ Grocery Total Calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int p1,n1,multiply;
+	printf("price per item");
+	scanf("%d",&p1);
+	printf ("number of item");
+	scanf("%d",&n1);
+	multiply=p1*n1;
+	printf ("calculate the total cost is %d",multiply);
+	return 0;
+}
