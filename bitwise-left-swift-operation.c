6161446f8e9228e7d2b190d6de23bce024596859
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-08-07 20:26:47 +0530

    Bitwise left shift operation program in c

---
+++ bitwise-left-swift-operation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c;
+	printf("enter first number");
+	scanf("%d",&a);
+	printf("enter second number");
+	scanf("%d",&b);
+	c=a<<b;
+	printf("oo bhai tera c=%d",c);
+	return 0;
+}
