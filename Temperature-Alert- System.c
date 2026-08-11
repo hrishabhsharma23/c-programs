2869efe18b36aa1f107dcdab6dc0fcf2e904b11c
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-08-11 19:24:03 +0530

    Temperature alert system program in c

---
+++ Temperature-Alert- System.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int t;
+	printf("enter the temperature");
+	scanf("%d",&t);
+	
+	if (t>40) 
+	printf("display the alert,alert");
+	
+	else 
+	printf("temperature is normal,normal");
+	
+	return 0;
+	
+}
