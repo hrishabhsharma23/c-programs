cb0ce9ffcbe647f5d8c44a3363f6e0cc629a4179
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-08-12 17:41:19 +0530

    Positive negative check by if else program in c

---
+++ positive-negative-check-by-if-else.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+	int n;
+	printf ("enter the number");
+	scanf("%d",&n);
+	
+	if (n==0);
+	{
+	printf(/n"%d is zero number" ,zero);
+}
+	else
+	{if (n>0);
+	printf(/n"%d is positive number",positive);
+	else 
+	printf(/n"%d is neegative number",negative);
+}
+	return 0;
+
+}
