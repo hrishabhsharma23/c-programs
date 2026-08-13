3c4f178afb4b66e382a10c32d2df365334bdd6ce
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-08-13 22:30:19 +0530

    Student paas or fail check program in c

---
+++ student-paas-or-fail-check.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int m;
+	printf("enter the marks");
+	scanf("%d",&m);
+	
+	if (m>=33) 
+	printf("student is pass ");
+	
+	else 
+	printf("student is fail");
+	
+	return 0;
+	
+}
