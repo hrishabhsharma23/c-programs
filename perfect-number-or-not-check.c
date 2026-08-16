fb557196a3a0f51f8fbd3df865ca600ec0b3d721
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-08-16 10:25:08 +0530

    Perfect number or not check program in c

---
+++ perfect-number-or-not-check.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+	int n,div=0,i,sum;
+	printf("enter the interger");
+	scanf("%d",&n);
+	i=1;
+	while (i<n)
+	{if (n%i=o)
+	{div++;
+}
+	  i=i+1;
+	}
+	sum=div+div;
+	{if sum=n
+	printf("the interger is perfect number",perfect number);
+	else
+	printf("the interger is not perfect number",perfect number);
+	}
+	return 0;
+}
