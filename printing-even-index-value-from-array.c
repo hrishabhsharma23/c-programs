7f15b6966e7f4fa661dd82b5048985cd6686ba52
Hrishabh Sharma <hrishabh.sharma2025@rnbglobal.ac.in>
2026-09-22 22:35:12 +0530

    Printing even index value from array program in c

---
+++ printing-even-index-value-from-array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int value[10],i;
+	printf("enter 10 values in array \n");
+	for (i=0;i<10;i++)
+	{scanf("%d",&value[i]);
+	}
+	printf(" values on even index in array");
+	for (i=0;i<10;i++)
+	{
+		if (i%2!=0)
+		printf("%d",value[i]);
+	}
+	
+	return 0;
+}
